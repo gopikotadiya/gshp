f9b769afc7dbd7cfd213c9cd7c2e6098c2c27665
Gopi Kotadiya <153471783+gopikotadiya@users.noreply.github.com>
2025-03-07 09:43:32 -0800

    removed unwanted pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
-import Login from './pages/Login';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Register from './pages/Register';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
